73814145d9b14cfe5b5a8da7af67e34f47432ce6
mkdir-KumarAnupam <kranupam101@gmail.com>
2026-04-20 07:41:32 +0530

    aws
    aws

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # CodeDeploy automatically runs this as part of the AfterInstall hook
-cd /home/ubuntu/simulator
+cd /home/ec2-user/simulator
 
 # Provide full path to bun because CodeDeploy shells often lack environment variables
-/home/ubuntu/.bun/bin/bun install
+/home/ec2-user/.bun/bin/bun install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # CodeDeploy automatically runs this as part of the ApplicationStart hook
-cd /home/ubuntu/simulator
+cd /home/ec2-user/simulator
 
-export PATH="/home/ubuntu/.bun/bin:$PATH"
+export PATH="/home/ec2-user/.bun/bin:$PATH"
 
 # Restart the application using PM2
 pm2 restart simulator || pm2 start server/src/index.js --interpreter bun --name "simulator"
